cf6e4fbf969801d293bd7a2d47f29324a1a2d76b
itggot-Peter-Hellstrom <peter.hellstrom@itggot.se>
2018-04-19 10:46:01 +0200

    Registrering
    La till olika views och kodade början på mitt registreringssystem.

--- app.rb
+++ app.rb
@@ -1,10 +1,73 @@
 class App < Sinatra::Base
-require 'sinatra'
-require 'sqlite3'
-		
-	get('/') do
-		tja
+	require 'sinatra'
+	require 'sqlite3'
+	enable :sessions
+		get('/') do
+			slim(:index, locals:{ greeting: "Hello from index!" })
+		end
+		get('/register') do
+			slim(:register)
+		end	  
+		get('/error') do
+			slim(:error)
+		end
+		def set_error(error_message)
+			session[:error] = error_message
+		end
+	
+		def get_error()
+			error = session[:error]
+			session[:error] = nil
+			return error
+		end
+		post('/register') do
+			db = SQLite3::Database.new('db/db.db')
+			db.results_as_hash = true
+			
+			username = params["username"]
+			password = params["password"]
+			password_confirmation = params["password2"]
+			
+			result = db.execute("SELECT id FROM users WHERE username=?", [username])
+			
+			if result.empty?
+				if password == password_confirmation
+					password_digest = BCrypt::Password.create(password)
+					
+					db.execute("INSERT INTO users(username, password_digest) VALUES (?,?)", [username, password_digest])
+					redirect('/')
+				else
+					set_error("Passwords don't match")
+					redirect('/error')
+				end
+			else
+				set_error("Username already exists")
+				redirect('/error')
+			end
+			
+		end
+	
+		post('/login') do
+			db = SQLite3::Database.new('db/db.db')
+			db.results_as_hash = true
+			username = params["username"]
+			password = params["password"]
+			
+			result = db.execute("SELECT id, password_digest FROM users WHERE username=?", [username])
+	
+			if result.empty?
+				set_error("Invalid Credentials")
+				redirect('/error')
+			end
+	
+			user_id = result.first["id"]
+			password_digest = result.first["password_digest"]
+			if BCrypt::Password.new(password_digest) == password
+				session[:user_id] = user_id
+				redirect('/notes')
+			else
+				set_error("Invalid Credentials")
+				redirect('/error')
+			end
+		end
 	end
-		
-
-end
