538b4846e309d830d0779598c7372edb2244c2a1
itggot-Peter-Hellstrom <peter.hellstrom@itggot.se>
2018-05-02 20:09:23 +0200

    listor
    fixade så man kan lägga till listor och lägga till saker i listan

--- app.rb
+++ app.rb
@@ -24,8 +24,19 @@ class App < Sinatra::Base
 		get('/notes') do
 			db = SQLite3::Database.new('db/db.db')
 			user = db.execute("SELECT * FROM users WHERE id=?", session[:user_id])
-			p user
-			slim(:notes, locals:{session:session, user:user})
+			lists = db.execute("SELECT * FROM listor WHERE user_id=?", session[:user_id])
+			list_items = db.execute("SELECT * FROM list_objekt WHERE user_id=?", session[:user_id])
+			slim(:notes, locals:{session:session, user:user, lists:lists, list_items:list_items})
+		end
+		post('/note') do
+			db = SQLite3::Database.new('db/db.db')
+			db.execute("INSERT INTO list_objekt (namn, lista, mängd, user_id) VALUES(?,?,?,?)",[params[:list_item],params[:lista], params[:ammount], session[:user_id] ] )
+			redirect("/notes")
+		end
+		post("/add_list") do
+			db = SQLite3::Database.new('db/db.db')
+			db.execute("INSERT INTO listor (namn, user_id) VALUES(?,?)", [params[:list_name], session[:user_id]] )
+			redirect("/notes")
 		end
 		
 		post('/register') do
@@ -64,7 +75,7 @@ class App < Sinatra::Base
 			result = db.execute("SELECT id, password_digest FROM users WHERE username=?", [username])
 	
 			if result.empty?
-				set_error("Invalid Credentials")
+				set_error("Invalid Username ")
 				redirect('/error')
 			end
 	
@@ -74,7 +85,7 @@ class App < Sinatra::Base
 				session[:user_id] = user_id
 				redirect('/notes')
 			else
-				set_error("Invalid Credentials")
+				set_error("Invalid Password")
 				redirect('/error')
 			end
 		end
